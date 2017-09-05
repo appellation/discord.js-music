9c581adb257edec8cf13e941dbce140ce28bb5fa
Will Nelson <nelson.will@live.com>
2017-09-04 19:25:33 -0700

    actually remove dist

--- dist/Client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Client {
-    constructor(client, services) {
-        this.playlists = new Map();
-        this.discord = client;
-    }
-}
-exports.default = Client;
