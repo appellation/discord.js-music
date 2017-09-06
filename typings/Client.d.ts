487fa77138d26d872faf47d78ae8853a045d8618
Will Nelson <nelson.will@live.com>
2017-09-05 18:01:57 -0700

    remove typings

--- typings/Client.d.ts
+++
@@ -1,8 +0,0 @@
-import { IService } from 'cassette';
-import { Client as Discord, Snowflake } from 'discord.js';
-import Playlist from './Playlist';
-export default class Client {
-    readonly discord: Discord;
-    readonly playlists: Map<Snowflake, Playlist>;
-    constructor(client: Discord, services: IService[]);
-}
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './Client';
-export * from './Error';
-export * from './Playlist';
